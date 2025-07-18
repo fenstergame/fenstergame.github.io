29faf1df7c2a445a5122393b5256386aaaed3f5a
Florian Lederer <75954553+ledererflorian@users.noreply.github.com>
2025-07-18 17:53:04 +0200

    dfghgfhf

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
+//asdfasdfsadf
 export default defineConfig({
   plugins: [react()],
   base: "/fenstergame.github.io/",
